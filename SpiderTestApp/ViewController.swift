6005a524fafe8125f15e3f5c1e14eab82bf5e6e0
MiZaft <mi.zaft1@gmail.com>
2024-08-20 18:29:06 +0300

    Initial Commit

---
+++ SpiderTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpiderTestApp
+//
+//  Created by Максим Евграфов on 20.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
